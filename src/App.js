b2a5059f517bc4d3c4d73f1c5aee9eca8bcc61fc
rajeendra <rajeendra.kanishka@gmail.com>
2022-12-10 07:20:30 +0530

    Component <RouterApp /> added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 
 import React from 'react';
-import CardApp from "./apps/card/CardApp";
-//import Tenzies from './apps/tenzies/Tenzies';
+// import CardApp from "./apps/card/CardApp";
+// import Tenzies from './apps/tenzies/Tenzies';
+import RouterApp from './apps/router/RouterApp';
 //import './App.css'
 
 function App() {
 
   return (
-    <CardApp />
+    // <CardApp />
     // <Tenzies />
+    <RouterApp />
   );
 }
 
