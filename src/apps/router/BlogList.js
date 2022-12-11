0403b07a76ac93ca4d3f05e57c1d434fdea48a1c
rajeendra <rajeendra.kanishka@gmail.com>
2022-12-11 09:56:18 +0530

    Modifications to  <RouterApp />

--- src/apps/router/BlogList.js
+++ src/apps/router/BlogList.js
@@ -7,8 +7,8 @@ const BlogList = ({ blogs }) => {
         <div className="blog-preview" key={blog.id} >
           <Link to={`/blogs/${blog.id}`}>
             <h2>{ blog.title }</h2>
+            <p>Written by { blog.author }</p>
           </Link>
-          <p>Written by { blog.author }</p>
         </div>
       ))}
     </div>
