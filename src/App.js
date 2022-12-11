c06a3fa0825c33c2fa3cf1aa9a1c41808cee5f66
rajeendra <rajeendra.kanishka@gmail.com>
2022-12-11 14:21:12 +0530

    Material UI Framwork and MUI components added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 
 import React from 'react';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 // import CardApp from "./apps/card/CardApp";
 // import Tenzies from './apps/tenzies/Tenzies';
-import RouterApp from './apps/router/RouterApp';
+// import RouterApp from './apps/router/RouterApp';
+// import RouterApp from './apps/router/RouterApp';
+import MUIApp from './apps/mui/MUIApp';
 //import './App.css'
 
 function App() {
@@ -10,7 +18,8 @@ function App() {
   return (
     // <CardApp />
     // <Tenzies />
-    <RouterApp />
+    // <RouterApp />
+    <MUIApp />
   );
 }
 
