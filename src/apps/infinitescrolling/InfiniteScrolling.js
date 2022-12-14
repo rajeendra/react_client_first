a44ab042a87f5f5d6e8a744c0faca84ecc98740f
rajeendra <rajeendra.kanishka@gmail.com>
2022-12-15 00:16:24 +0530

    app component <InfiniteScrolling  /> added

---
+++ src/apps/infinitescrolling/InfiniteScrolling.js
@@ -0,0 +1,152 @@
+import React, { useState, useRef, useCallback, useEffect } from 'react'
+import useBookSearch from './useBookSearch'
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+
+export default function InfiniteScrolling() {
+
+  const [query, setQuery] = useState('')
+  const [pageNumber, setPageNumber] = useState(1)
+  const [autoScroll, setAutoScroll] = useState(false)
+  //const [iterateToggle, setIterateToggle] = useState(false)
+
+  const {
+    books,
+    hasMore,
+    loading,
+    error
+  } = useBookSearch(query, pageNumber)
+
+  //let counter = 0
+  const counterRef = useRef(0)
+  // const iterateToggle = useRef(true)
+  // useEffect(()=>{
+  //   counterRef.current++;
+  // },[iterateToggle])
+
+  useEffect(()=>{
+    if (!autoScroll) return
+    if (loading) return
+    if (hasMore) handleClickScroll();
+  },[loading, hasMore])
+
+  function handleClickScroll(){
+    var element = document.querySelector(".last-element"); 
+    if (element==null) return;
+    element.scrollIntoView({ behavior: 'smooth', block: 'end'});
+  }
+
+  // The useRef Hook allows you to persist values between renders.
+  // It can be used to store a mutable value that does not cause a re-render when updated.
+  // It can be used to access a DOM element directly
+  const observer = useRef()
+
+  // The React useCallback Hook returns a memoized callback function.
+  // Think of memoization as caching a value so that it does not need to be recalculated.
+  // This allows us to isolate resource intensive functions so that they will not automatically run on every render.
+  // The useCallback Hook only runs when one of its dependencies update  
+  //    This function will fire last element rendering time for each fetch
+  const lastBookElementRef = useCallback(node => {
+    if (loading) return
+    if (observer.current) observer.current.disconnect()
+    // at Every fire, New IntersectionObserver will set in observer ref
+    observer.current = new IntersectionObserver(entries => {
+      // Intersection Observer trigger a Function when Scrolling to an Element
+      // Intersect is happen when scrollbar everytime hit the bottom
+      if (entries[0].isIntersecting && hasMore) {
+        // when scrollbar hit the bottom and if has more pages available to fetch
+        setPageNumber(prevPageNumber => prevPageNumber + 1)
+        // Note: set setPageNumber will cause to rerender and 
+        // so fetch happen with new page number and same Query string 
+      }
+    })
+    if (node) observer.current.observe(node)
+  // init loading - true // fetching loading - true // afterFetch loading - false 
+  // so loading state only change when fech started and fetch ends
+  // In here since loading==true just return Only effective when loading change from..
+  // .. true to false which is after each fetch 
+  // hasMore only change when there's no any pages left to fetch
+  }, [loading, hasMore]) // The useCallback Hook only runs when one of its dependencies update
+  
+  function handleSearch(e) {
+    // Each time serch text change consider as a new search so page number set to 1
+    setQuery(e.target.value)
+    setPageNumber(1)
+    //counterRef.current = 1;
+  }
+
+  function handleAutoScroll(e){
+    setAutoScroll(prevSetting => !prevSetting)
+    if (hasMore) {
+      setPageNumber(prevPageNumber => prevPageNumber + 1)
+    }    
+  }
+
+  const bookStyle = {
+    "marginLeft": "10px",  
+    "paddingTop": "90px"
+  }
+
+  const msgStyle = {
+    "marginLeft": "10px",  
+  } 
+
+  //const count = counterRef.current++;
+  
+  //const bookElements=
+
+  const bookElements =  books.map((book, index) => {
+    
+    if (books.length === index + 1) {
+      // ref - To access actual DOM nodes in React, we use the ref prop.
+      // Usually you would probably pass a ref object using createRef or 
+      // useRef but the ref prop can also take a function, this is referred to as a “callback ref”.
+      // This function will fire when the element mounts and unmounts from the DOM.
+      //const count = counterRef.current++;
+      //                                                 key should be unique
+      //                                                 Otherwise rendering not happen properly 
+      return <div className='last-element' ref={lastBookElementRef} key={book}> {index + 1}. {book} </div>
+    } else {
+      //  key should be unique
+      //  Otherwise rendering not happen properly 
+      return <div key={book}>{index + 1}. {book}</div>
+    }
+  })
+
+  return (
+    <>
+      {/* { console.log("bookElements.length " + bookElements.length) } */}
+      {/* <div onClick={handleClickScroll} >{new Date().getMinutes() }</div> */}
+      {/* <input type="text" value={query} onChange={handleSearch}></input> */}
+      <Box sx={{ position: 'fixed', width: '100%', display:'flex', gap: 1, backgroundColor: 'white'}}  >
+      <TextField
+              sx={{ m:2}} 
+              value={query}
+              onChange={handleSearch}
+              margin="normal"
+              required
+              fullWidth
+              id="search"
+              label="search text"
+              name="search"
+              autoComplete="search"
+              autoFocus
+            />
+            <Button
+              onClick={handleAutoScroll}
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2, mr: 2 }}
+            > { autoScroll ? "Auto: ON" : "Auto: OFF" }
+            </Button>
+      </Box>
+      <div style={bookStyle}>{ bookElements } </div>
+      <div style={msgStyle}>{loading && 'Loading...'}</div>
+      <div style={msgStyle}>{error && 'Error'}</div>
+    </>
+  )
+}
+
+
---
+++ src/apps/infinitescrolling/useBookSearch.js
@@ -0,0 +1,60 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+export default function useBookSearch(query, pageNumber) {
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(false)
+  const [books, setBooks] = useState([])
+  const [hasMore, setHasMore] = useState(false)
+  const limit = 100;
+  
+  // When changed the query (Search text)
+  // remove all books from the array
+  useEffect(() => {
+    setBooks([])
+  }, [query])
+
+  useEffect(() => {
+    setLoading(true)
+    setError(false)
+    //setBooks([])
+    let cancel
+    axios({
+      method: 'GET',
+      url: 'http://openlibrary.org/search.json',
+      params: { q: query, page: pageNumber, limit: limit},
+      cancelToken: new axios.CancelToken(c => cancel = c)
+    }).then(res => {
+      
+      let latestBooks = []
+
+      setBooks(prevBooks => {
+        //Here sorrounded by SET to make a unique book items
+        //Sinc we use book title as for the key of book component at render time..
+        //..book title should be unique   
+        return [...new Set([...prevBooks, ...res.data.docs.map(b => b.title)])]
+        
+        // No unique items maintaining
+        //return [...prevBooks, ...res.data.docs.map(b => b.title)]
+        
+        // -- This logic keep only latest fetched items limited to specified numbers in <limit>
+        // latestBooks = [...new Set([...prevBooks, ...res.data.docs.map(b => b.title)])]
+        // const slicePoint = latestBooks.length - ( limit * 2 )
+        // return (slicePoint>0) ? latestBooks.slice(slicePoint): latestBooks
+
+      })
+
+      setHasMore(res.data.docs.length > 0)
+      setLoading(false)
+    }).catch(e => {
+      if (axios.isCancel(e)) return
+      setError(true)
+    })
+    return () => cancel()
+  }, [query, pageNumber])
+
+  // After each fetch over we know  has More book to fetch or not
+  // If no any Error and books fetched Then outputting books and loading state
+  // Othewise if Error Then outputting error and loading state
+  return { loading, error, books, hasMore }
+}
